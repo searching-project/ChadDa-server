682e1047d0b4a73f09240f0ae990063276025e04
Hyemm <48474929+hm5938@users.noreply.github.com>
2022-10-01 16:42:34 +0900

    #1 -프로젝트 초기 설정

---
+++ src/test/java/com/search/instagramsearching/InstagramSearchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search.instagramsearching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramSearchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
