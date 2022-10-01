ea49dba1db30f454a0824ca5abc6795a634af4af
Hyemm <48474929+hm5938@users.noreply.github.com>
2022-10-01 17:13:46 +0900

    feature #3 - esception 초기설정

---
+++ src/main/java/com/search/instagramsearching/exception/CustomException.java
@@ -0,0 +1,16 @@
+package com.search.instagramsearching.exception;
+public class CustomException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
