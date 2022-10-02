be62d9d875f060e1eeeda81ce9bd7dcee0a70572
송민진 <m_6595@naver.com>
2022-10-03 03:03:27 +0900

    #3 feat : 유저 entity 타입 변경

--- src/main/java/com/search/instagramsearching/entity/Users.java
+++ src/main/java/com/search/instagramsearching/entity/Users.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.Date;
 
 @Getter
 @NoArgsConstructor
@@ -16,26 +15,34 @@ import java.util.Date;
 public class Users {
     @Id
     private Long sid;
+
     @Column
     private Long profileId;
+
     @Column
     private String profileName;
+
     @Column
     private String firstnameLastname;
+
     @Column
     private String description;
+
     @Column
     private int following;
+
     @Column
     private int followers;
+
     @Column
     private int nPosts;
+
     @Column
     private String url;
-    @Column
-    private Date cts;
-    @Column
-    private boolean isBusinessAccount;
 
+    @Column
+    private String cts;
 
+    @Column
+    private String isBusinessAccount;
 }
