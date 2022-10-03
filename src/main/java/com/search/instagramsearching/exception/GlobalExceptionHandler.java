88da74a66c5a99bd7d099e761704f8b286c5925e
송민진 <m_6595@naver.com>
2022-10-03 12:31:10 +0900

    feature #8 - GlobalExceptionHandler 생성

---
+++ src/main/java/com/search/instagramsearching/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.search.instagramsearching.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    
+}
