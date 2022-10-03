aad8ee78d8e1c97623e047cf7a2facbccd9e33f5
송민진 <m_6595@naver.com>
2022-10-04 00:40:44 +0900

    feature #7 UsersService 구현

--- src/main/java/com/search/instagramsearching/service/UsersService.java
+++ src/main/java/com/search/instagramsearching/service/UsersService.java
@@ -1,13 +1,37 @@
 package com.search.instagramsearching.service;
 
+import com.search.instagramsearching.entity.Users;
+import com.search.instagramsearching.exception.ResultNotFoundException;
 import com.search.instagramsearching.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 @Slf4j
 public class UsersService {
     private final UsersRepository usersRepository;
+
+    @Transactional
+    public List<Users> naturalSearch(String keyword, Pageable pageable) {
+        List<Users> result = usersRepository.ngramNatualSearch(keyword, pageable);
+        if (result == null || result.size() == 0) {
+            throw new ResultNotFoundException();
+        }
+        return result;
+    }
+
+    @Transactional
+    public List<Users> booleanSearch(String keyword, Pageable pageable) {
+        List<Users> result = usersRepository.ngramNatualSearch(keyword, pageable);
+        if (result == null || result.size() == 0) {
+            throw new ResultNotFoundException();
+        }
+        return result;
+    }
 }
