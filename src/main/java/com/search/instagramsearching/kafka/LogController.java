7f6d5a9dba61a7473a666aea156a4086c64bc86e
송민진 <m_6595@naver.com>
2022-10-19 04:03:00 +0900

    feature #27 임시 kafka 설정 완료 - Sprnig과 브로커 연결 필요

---
+++ src/main/java/com/search/instagramsearching/kafka/LogController.java
@@ -0,0 +1,19 @@
+package com.search.instagramsearching.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("logging")
+public class LogController {
+    private static final Logger kafkaLogger = LoggerFactory.getLogger("kafkaLogger");
+
+    @GetMapping("test")
+    public void testLogging(@RequestParam(value = "name")String name) {
+        kafkaLogger.info("name: {}", name);
+    }
+}
