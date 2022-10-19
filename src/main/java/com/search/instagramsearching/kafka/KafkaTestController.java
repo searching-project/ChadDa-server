5cce1ced252cf2dc3f2330e2619eef68f6ba1260
송민진 <m_6595@naver.com>
2022-10-19 22:22:26 +0900

    feature #27 logback-kafka-appender 적용 test 완료

--- src/main/java/com/search/instagramsearching/kafka/KafkaTestController.java
+++ src/main/java/com/search/instagramsearching/kafka/KafkaTestController.java
@@ -9,13 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("logging")
-public class LogController {
-    private static final Logger kafkaLogger = LoggerFactory.getLogger("kafkaLogger");
-//    private LogProducer logProducer;
+public class KafkaTestController {
+
+//     // 방법 1. Logback 기본 Layout 활용해서 kafka에 전달
+//     private static final Logger kafkaLogger = LoggerFactory.getLogger("kafkaLogger");
+     private static final Logger kafkaLogger = LoggerFactory.getLogger("kafkaAppender");
+
+//    // 방법 2. Logstash Layout 활용
+//    private static final Logger kafkaLogger = LoggerFactory.getLogger("logstashKafkaAppender");
 
     @GetMapping("test")
     public void testLogging(@RequestParam(value = "name")String name) {
         kafkaLogger.info("name: {}", name);
-//        logProducer();
     }
 }
