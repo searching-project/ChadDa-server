a3b43928ba2f0112d433f80a07c727b436b47765
Hyemm <48474929+hm5938@users.noreply.github.com>
2022-10-20 15:55:45 +0900

    redis 사용을 위한 cache enable

--- src/main/java/com/search/instagramsearching/InstagramSearchingApplication.java
+++ src/main/java/com/search/instagramsearching/InstagramSearchingApplication.java
@@ -2,8 +2,12 @@ package com.search.instagramsearching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableCaching
 public class InstagramSearchingApplication {
 
     public static void main(String[] args) {
